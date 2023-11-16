02bbcbb6aa1a778a125c5ce502b7a4ad4748cb9b
Yutivich TUNUENG <yutivich2005@gmail.com>
2023-11-16 11:34:34 +0700

    OO-style run_ball.py

--- run_ball.py
+++ run_ball.py
@@ -1,27 +1,29 @@
 import turtle
 import ball
 
-num_balls = int(input("Number of balls to simulate: "))
-turtle.speed(0)
-turtle.tracer(0)
-turtle.hideturtle()
-canvas_width = turtle.screensize()[0]
-canvas_height = turtle.screensize()[1]
-ball_radius = 0.05 * canvas_width
-turtle.colormode(255)
-color_list = []
-xpos = []
-ypos = []
-vx = []
-vy = []
-ball_color = []
-ball.initilizing(xpos, ypos, vx, vy, ball_color, canvas_width, canvas_height, ball_radius, num_balls)
-while (True):
-    turtle.clear()
-    for i in range(num_balls):
-        ball.draw_circle(ball_color[i], ball_radius, xpos[i], ypos[i])
-        ball.move_circle(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius)
-    turtle.update()
 
-# hold the window; close it by clicking the window close 'x' mark
-turtle.done()
+class Simulator:
+    def __init__(self, num_balls):
+        self.num_balls = num_balls
+
+    def display(self):
+        turtle.speed(0)
+        turtle.tracer(0)
+        turtle.hideturtle()
+        canvas_width = turtle.screensize()[0]
+        canvas_height = turtle.screensize()[1]
+        ball_radius = 0.05 * canvas_width
+        turtle.colormode(255)
+        all_ball = ball.Ball(canvas_width, canvas_height, ball_radius, num_balls)
+        all_ball.create_balls()
+        while True:
+            turtle.clear()
+            for i in range(num_balls):
+                all_ball.draw_circle(i)
+                all_ball.move_circle(i)
+            turtle.update()
+        turtle.done()
+
+
+num_balls = int(input("Number of balls to simulate: "))
+Simulator(num_balls).display()
